dd836835d35b2bba8b35a7e37cab8235441b57e8
shamrice <shamrice@gmail.com>
2020-12-28 00:42:16 -0500

    updating how command line arguements are parsed and handled.

--- source/crssr.cbl
+++ source/crssr.cbl
@@ -1,7 +1,7 @@
       *>*****************************************************************
       *> Author: Erik Eriksen
       *> Create Date: 2020-11-05
-      *> Last Updated: 2020-12-27
+      *> Last Updated: 2020-12-28
       *> Purpose: Application entry point
       *> Tectonics:
       *>     ./build.sh
@@ -21,15 +21,27 @@
 
        working-storage section.
 
-       77 command-arguments                    pic x(2048) value spaces.
+       01  parsed-cmd-params.
+           05  valid-param-sw                  pic a value 'N'.
+               88  is-valid-param              value 'Y'.
+               88  not-valid-param             value 'N'.
+           05  refresh-feed-sw                 pic a value 'Y'.
+               88  is-refresh                  value 'Y'.
+               88  not-refresh                 value 'N'.
+           05  add-feed-sw                     pic a value 'N'.
+               88  is-add-feed                 value 'Y'.
+               88  not-add-feed                value 'N'.
+           05  interactive-mode-sw             pic a value 'N'.
+               88  is-interactive              value 'Y'.
+               88  not-interactive             value 'N'.
 
-       77 is-valid-param                       pic a value 'N'.
+       77  cmd-args-buffer                     pic x(2048) value spaces.
 
-       78 new-line                             value x"0a".
+       78  new-line                            value x"0a".
 
-       78 program-version                      value __APP_VERSION.
-       78 web-url  value __SOURCE_URL.
-       78 build-date                           value __BUILD_DATE.
+       78  program-version                     value __APP_VERSION.
+       78  web-url value __SOURCE_URL.
+       78  build-date                          value __BUILD_DATE.
 
 
        procedure division.
@@ -44,31 +56,58 @@
                new-line "Build Date: " build-date new-line 
            end-display
 
-           accept command-arguments from command-line end-accept
+           accept cmd-args-buffer from command-line 
+           perform parse-cmd-args
           
-      *> No where near perfect... but will at least check if user has some url in their input.
-           if command-arguments(1:4) = "http" or "HTTP" then
-               move 'Y' to is-valid-param
+
+           if is-add-feed then 
                display 
                    "Downloading and parsing RSS feed: " 
-                   function trim(command-arguments)
+                   function trim(cmd-args-buffer)
                end-display
-               call "rss-downloader" using by content command-arguments 
-               display "Downloading and parsing complete."       
+               call "rss-downloader" using by content cmd-args-buffer
+              
+               display "Downloading and parsing complete."   
+           end-if    
+  
+           if is-interactive then 
+               call "rss-reader-menu" using by content refresh-feed-sw
            end-if
 
-           if command-arguments = spaces then
-               move 'Y' to is-valid-param
-               display "Launching application in interactive mode..."
-               call "rss-reader-menu"
-           end-if
-
-           if is-valid-param = 'N' then
+           if not-valid-param then
                perform print-help
            end-if
      
            display "End program."
            stop run.
+
+
+       parse-cmd-args.
+
+      *> No where near perfect... but will at least check if user has some url in their input.
+           if cmd-args-buffer(1:4) = "http" or "HTTP" then
+               move 'Y' to valid-param-sw
+               move 'Y' to add-feed-sw
+               exit paragraph      
+           end-if
+
+      * If user specifies to not refresh feeds at start, set flag.
+           if cmd-args-buffer(1:12) = "--no-refresh" then
+               display "Not refreshing RSS Feeds."
+               move 'Y' to valid-param-sw 
+               move 'N' to refresh-feed-sw 
+               move 'Y' to interactive-mode-sw
+               exit paragraph           
+           end-if
+
+           if cmd-args-buffer = spaces  then
+               move 'Y' to valid-param-sw
+               move 'Y' to interactive-mode-sw
+               display "Launching application in interactive mode..."
+               exit paragraph
+           end-if
+           
+           exit paragraph.
 
        print-help.
 
@@ -77,8 +116,14 @@
                "COBOL." new-line new-line
                "Add new feed:" new-line
                "  Usage: crssr [url of rss feed]" new-line new-line
+               "Start interactive mode without refreshing feeds:"
+               new-line 
+               "  Usage: crssr --no-refresh" new-line new-line
                "Run application with no arguments to start application "
-               "in interactive mode." new-line
-           end-display.
+               "in interactive mode and refresh feeds." new-line
+               new-line  
+           end-display
+
+           exit paragraph.
 
        end program cobol-rss-reader.
