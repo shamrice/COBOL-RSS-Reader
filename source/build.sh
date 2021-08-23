42ead63d4ca2a018bba0ea8400239854295001a1
shamrice <shamrice@gmail.com>
2021-08-23 18:09:29 -0400

    #3 : Updating build version with mouse changes.

--- source/build.sh
+++ source/build.sh
@@ -8,7 +8,7 @@
 # set back to original placeholder values.
 #
 
-APP_VERSION=\"v0.37\"
+APP_VERSION=\"v0.38\"
 SOURCE_URL=\"https\\:\\/\\/github.com\\/shamrice\\/COBOL-RSS-Reader\"
 CUR_BUILD_DATE=\"$(date +%Y-%m-%d)\"
 
