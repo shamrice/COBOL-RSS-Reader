1d5ca8f27e636d9a2b12c33fe7dfc0cfa4b5c2bb
shamrice <shamrice@gmail.com>
2021-09-20 03:32:58 -0400

    #7 : Adding the ability to add a '--debug' parameter to the build script to build in debug mode.

--- source/build.sh
+++ source/build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 #
-# Last updated: 2021-08-23
+# Last updated: 2021-09-20
 #
 # Builds sets placeholder values in source code based on variables at start of script 
 # and compiles the application. Once compilation step is completed, placeholders are 
 # set back to original placeholder values.
 #
 
-APP_VERSION=\"v0.38\"
+
+APP_VERSION=\"v0.39\"
 SOURCE_URL=\"https\\:\\/\\/github.com\\/shamrice\\/COBOL-RSS-Reader\"
 CUR_BUILD_DATE=\"$(date +%Y-%m-%d)\"
 
@@ -18,6 +19,16 @@ echo --------------------------
 echo Version: $APP_VERSION 
 echo Build Date: $CUR_BUILD_DATE
 echo 
+
+DEBUG=""
+
+if [[ "$1" == "--debug" ]]
+then 
+    DEBUG=$1
+    echo Debug flag set. Adding $1 to build arguements. 
+fi 
+
+
 echo Replacing placeholders in source with values for build process.
 sed -i "s/__APP_VERSION/$APP_VERSION/" crssr.cbl
 sed -i "s/__SOURCE_URL/$SOURCE_URL/" crssr.cbl
@@ -27,7 +38,12 @@ sed -i "s/__SOURCE_URL/$SOURCE_URL/" rss_reader_help.cbl
 sed -i "s/__BUILD_DATE/$CUR_BUILD_DATE/" rss_reader_help.cbl
 
 echo Compiling...
-cobc -O -x crssr.cbl rss_parser.cbl rss_reader_menu.cbl rss_reader_view_feed.cbl rss_reader_view_item.cbl rss_downloader.cbl browser_launcher.cbl rss_reader_add_feed.cbl rss_reader_delete_feed.cbl rss_reader_help.cbl rss_reader_export_feed.cbl rss_report_writer.cbl remove_rss_record.cbl logger.cbl string_helpers.cbl application_configurator.cbl reset_files.cbl cobweb-pipes.cob -o crssr 
+echo 
+BUILD_STRING="cobc -O2 -fstatic-call -x $DEBUG crssr.cbl rss_parser.cbl rss_reader_menu.cbl rss_reader_view_feed.cbl rss_reader_view_item.cbl rss_downloader.cbl browser_launcher.cbl rss_reader_add_feed.cbl rss_reader_delete_feed.cbl rss_reader_help.cbl rss_reader_export_feed.cbl rss_report_writer.cbl remove_rss_record.cbl logger.cbl string_helpers.cbl application_configurator.cbl reset_files.cbl cobweb-pipes.cob -o crssr"
+echo $BUILD_STRING 
+$BUILD_STRING
+echo 
+
 
 echo Setting placeholders back to placeholder names for next build.
 sed -i "s/$APP_VERSION/__APP_VERSION/" crssr.cbl
